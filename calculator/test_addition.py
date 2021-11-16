0dfeeb518d5491671b422e249179a19b571116ba
viseth89 <viseth89@gmail.com>
2021-11-15 21:21:31 -0600

    Added base addition file and pytest.ini

---
+++ calculator/test_addition.py
@@ -0,0 +1,10 @@
+from src.calculator import add
+import pytest
+
+def test_add():
+    result = add(3,4)
+    assert result == 7
+    
+def test_add_string():
+    with pytest.raises(TypeError):
+        add("string", 4)
